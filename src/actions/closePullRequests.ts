b3e5677e3abfa6d9172ebbccaef7cbd41403bd0f
nownabe <nownabe@users.noreply.github.com>
2022-03-07 16:35:15 +0900

    Implement closePullRequests action

--- src/actions/closePullRequests.ts
+++ src/actions/closePullRequests.ts
@@ -1,10 +1,27 @@
 import type { ActionBuilder } from ".";
 
 const builder: ActionBuilder = async (octokit) => {
-  return async (repo) => {
-    const sleep = () =>
-      new Promise<void>((resolve) => setTimeout(() => resolve(), 1000));
-    await sleep();
+  return async (repo, updateText) => {
+    updateText("closing pull requests");
+    const pulls = await octokit.paginate("GET /repos/{owner}/{repo}/pulls", {
+      owner: repo.owner.login,
+      repo: repo.name,
+      state: "open",
+      per_page: 100,
+    });
+
+    for (let i = 0; i < pulls.length; i++) {
+      updateText(`closing pull requests (${i + 1}/${pulls.length})`);
+
+      const pull = pulls[i];
+
+      await octokit.request("PATCH /repos/{owner}/{repo}/pulls/{pull_number}", {
+        owner: repo.owner.login,
+        repo: repo.name,
+        pull_number: pull.number,
+        state: "closed",
+      });
+    }
   };
 };
 
