4d045eb6b802875e93185d160a37b938c631033c
nownabe <nownabe@users.noreply.github.com>
2022-03-07 16:27:57 +0900

    Implement closeIssues action

--- src/actions/closeIssues.ts
+++ src/actions/closeIssues.ts
@@ -1,10 +1,30 @@
 import type { ActionBuilder } from ".";
 
 const builder: ActionBuilder = async (octokit) => {
-  return async (repo) => {
-    const sleep = () =>
-      new Promise<void>((resolve) => setTimeout(() => resolve(), 1000));
-    await sleep();
+  return async (repo, updateText) => {
+    updateText("closing issues");
+    const issues = await octokit.paginate("GET /repos/{owner}/{repo}/issues", {
+      owner: repo.owner.login,
+      repo: repo.name,
+      state: "open",
+      per_page: 100,
+    });
+
+    for (let i = 0; i < issues.length; i++) {
+      updateText(`closing issues (${i + 1}/${issues.length})`);
+
+      const issue = issues[i];
+
+      await octokit.request(
+        "PATCH /repos/{owner}/{repo}/issues/{issue_number}",
+        {
+          owner: repo.owner.login,
+          repo: repo.name,
+          issue_number: issue.number,
+          state: "closed",
+        }
+      );
+    }
   };
 };
 
