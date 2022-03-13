b411c1b32ea41385d814fb8b663eb31ed4903fab
nownabe <nownabe@users.noreply.github.com>
2022-03-13 15:15:29 +0900

    Filter repositories

--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,112 @@ const patPrompt = async (): Promise<string> => {
   return result.pat || process.env.GH_PAT;
 };
 
+type Filter = {
+  visibility?: "public" | "private" | null;
+  fork?: boolean | null;
+  archived?: boolean | null;
+  open_issues_count?: boolean | null;
+};
+
+type FilterFunc = (repo: Repo) => boolean;
+
+const filterPrompt = async (): Promise<FilterFunc> => {
+  const useFilter = await inquirer.prompt<{ useFilter: boolean }>([
+    {
+      type: "confirm",
+      name: "useFilter",
+      message: "Filter repositories by properties?",
+      default: false,
+    },
+  ]);
+
+  if (!useFilter.useFilter) {
+    return (_repo: Repo) => true;
+  }
+
+  const filter = await inquirer.prompt<Filter>([
+    {
+      type: "list",
+      name: "visibility",
+      message: "Filter by visibility?",
+      choices: [
+        { name: "No", value: null },
+        { name: "Only show public repositories", value: "public" },
+        { name: "Only show private repositories", value: "private" },
+      ],
+      default: null,
+    },
+    {
+      type: "list",
+      name: "fork",
+      message: "Filter by fork or not?",
+      choices: [
+        { name: "No", value: null },
+        { name: "Only show fork repositories", value: true },
+        { name: "Only show non-fork repositories", value: false },
+      ],
+      default: null,
+    },
+    {
+      type: "list",
+      name: "archived",
+      message: "Filter by archived or not?",
+      choices: [
+        { name: "No", value: null },
+        { name: "Only show archived repositories", value: true },
+        { name: "Only show not archived repositories", value: false },
+      ],
+      default: null,
+    },
+    {
+      type: "list",
+      name: "open_issue_count",
+      message: "Filter by open issue count?",
+      choices: [
+        { name: "No", value: null },
+        {
+          name: "Only show respositories which have no open issues",
+          value: true,
+        },
+        { name: "Only show repositories which have open issues", value: false },
+      ],
+      default: null,
+    },
+  ]);
+
+  const filterFunc = (repo: Repo): boolean => {
+    let ok = true;
+
+    if (filter.visibility !== null) {
+      ok &&= repo.visibility === filter.visibility;
+    }
+
+    if (filter.fork !== null) {
+      ok &&= repo.fork === filter.fork;
+    }
+
+    if (filter.archived !== null) {
+      ok &&= repo.archived === filter.archived;
+    }
+
+    if (filter.open_issues_count !== null) {
+      if (filter.open_issues_count) {
+        ok &&= repo.open_issues_count === 0;
+      } else {
+        ok &&= repo.open_issues_count !== 0;
+      }
+    }
+
+    return ok;
+  };
+
+  return filterFunc;
+};
+
 const getCandidates = async (
   octokit: Octokit,
-  { targetOwner, isOwnerUser }: Actions
+  { targetOwner, isOwnerUser }: Actions,
+  filter: FilterFunc
 ): Promise<Repo[]> => {
   const spinner = new Spinner("%s Getting repositories...");
   spinner.start();
@@ -46,7 +149,7 @@ const getCandidates = async (
 
   spinner.stop(true);
 
-  return repos;
+  return repos.filter(filter);
 };
 
 const forkSymbol = " [fork]";
@@ -60,7 +163,7 @@ const reposPrompt = async (candidates: Repo[]): Promise<Repo[]> => {
     {
       type: "checkbox",
       name: "repositories",
-      message: "Choose repositories you want to organize.",
+      message: "Choose repositories to organize.",
       choices: candidates.map((c) => {
         const base = ` ${c.full_name}${c.fork ? forkSymbol : ""}`;
         const space = " ".repeat(indent - base.length);
@@ -109,7 +212,8 @@ const organize = async (actions: Actions, repos: Repo[]) => {
 };
 
 const getRepos = async (octokit: Octokit, actions: Actions) => {
-  const candidates = await getCandidates(octokit, actions);
+  const filter = await filterPrompt();
+  const candidates = await getCandidates(octokit, actions, filter);
   if (candidates.length === 0) {
     return [];
   }
